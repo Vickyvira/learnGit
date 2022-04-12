bcc6f8cbc8752e025f0a9b68d4cb38dceea4cb08
Vicky Kumar Das <vk9793@gmail.com>
2022-04-12 13:09:49 +0530

    added implementation for Diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,8 @@
 public class Diff {
-    
+    public static void main(String[] args) {
+        int a = 32;
+        int b = 12;
+
+        System.out.println("Dividing two numbers" + " " + a/b);
+    }
 }
